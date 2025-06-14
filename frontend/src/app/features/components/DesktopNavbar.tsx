7c566ff39eccffed42c21252d6cd6721f18921a6
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-06-14 19:13:14 +0530

    Redirect to home

--- frontend/src/app/features/components/DesktopNavbar.tsx
+++ frontend/src/app/features/components/DesktopNavbar.tsx
@@ -2,12 +2,13 @@
 import { ChevronDown, Download } from 'lucide-react'
 import React from 'react'
 import DropdownEvents from './DropdownEvents'
+import Link from 'next/link'
 
 const DesktopNavbar = ({onContactClick}: {onContactClick: () => void}) => {
   return (
     <div className='flex items-center gap-3 justify-between'>
         {/* Home */}
-    <div className='hover:bg-[#212840] px-1 py-8 text-[#7c859a] uppercase text-base tracking-tighter font-semibold '>Home</div>
+    <Link href="/" className='hover:bg-[#212840] px-1 py-8 text-[#7c859a] uppercase text-base tracking-tighter font-semibold '>Home</Link>
      {/* Events */}
       <DropdownEvents/>
 
@@ -15,12 +16,14 @@ const DesktopNavbar = ({onContactClick}: {onContactClick: () => void}) => {
        <div className='hover:bg-[#212840] px-1 py-8 text-[#7c859a] uppercase text-base tracking-tighter font-semibold flex items-center gap-0.5 '>Community
         <ChevronDown className='w-5'/>
       </div>
-        {/* Membership */}
-          <div className='hover:bg-[#212840] px-1 py-8 text-[#7c859a] uppercase text-base tracking-tighter font-semibold '>Membership</div>
-        {/* Gallery */}
-          <div className='hover:bg-[#212840] px-1 py-8 text-[#7c859a] uppercase text-base tracking-tighter font-semibold '>Gallery</div>
-          {/* More */}
-                 <div className='hover:bg-[#212840] px-1 py-8 text-[#7c859a] uppercase text-base tracking-tighter font-semibold flex items-center gap-0.5 '>More
+      {/* Membership */}
+      <Link href="/" className='hover:bg-[#212840] px-1 py-8 text-[#7c859a] uppercase text-base tracking-tighter font-semibold '>Membership
+      </Link>
+      {/* Gallery */}
+      <Link href="/" className='hover:bg-[#212840] px-1 py-8 text-[#7c859a] uppercase text-base tracking-tighter font-semibold '>Gallery
+      </Link>
+      {/* More */}
+      <div className='hover:bg-[#212840] px-1 py-8 text-[#7c859a] uppercase text-base tracking-tighter font-semibold flex items-center gap-0.5 '>More
         <ChevronDown className='w-5'/>
       </div>
       {/* Button */}
