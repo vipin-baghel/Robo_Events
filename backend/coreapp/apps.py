20d056255624949a30ff60d7ab24e5ba8334654f
Vipin Baghel <vipinbaghel1999@gmail.com>
2025-06-16 00:20:19 +0530

    .

--- backend/coreapp/apps.py
+++ backend/coreapp/apps.py
@@ -1,7 +1,4 @@
 from django.apps import AppConfig
-from django.conf import settings
-from django.contrib.sites.models import Site
-
 
 class CoreappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
